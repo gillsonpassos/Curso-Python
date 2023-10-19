c1678954e9e4bf2bf3447ec15862c5d953ca45b0
gillsonpassos <oliveirapassos07@outlook.com>
2023-10-19 10:27:26 -0300

    aulas de python

---
+++ aula1.py
@@ -0,0 +1,11 @@
+"""
+DocString
+para escrever varias linhas tipo comentarios
+usa no Python essas aspas triplas.
+"""
+
+
+# => Permite escrever  um comentario so em uma linha
+print(123) # => comentario na frente
+# comenta abaixo
+print(456)
---
+++ aula2.py
@@ -0,0 +1,5 @@
+# \r\n windows -> CRLF
+# \n linux -> LF
+print(12, 34, 1011, sep='-')
+print(56, 78, sep='-')
+print(9, 10, sep='-')
