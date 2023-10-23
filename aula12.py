576a46c3192f812e7be38f2f9f1d6bd0b872a1ba
gillsonpassos <oliveirapassos07@outlook.com>
2023-10-23 09:16:13 -0300

    Exercicios de calculo com python

---
+++ aula12.py
@@ -0,0 +1,11 @@
+nome = 'Gilson Passos'
+altura = 1.70
+peso = 80
+imc = peso / (altura * altura)
+print(nome, 'tem', altura, 'de altura,',)
+print('pesa', peso,  'quilos e seu imc é',)
+print(imc)
+
+# gilson Passos tem 1.70 de altura
+# pesa 80 quilos e seu IMC é
+# 27.68166089965398
