7ca7b81ea3cd82d4e5088265d7dc9154e2fbfc01
gillsonpassos <oliveirapassos07@outlook.com>
2023-11-01 09:52:36 -0300

    introdição aos blocos de código + if/ elif / else

---
+++ aula16.py
@@ -0,0 +1,10 @@
+# if  / elif      / else
+# se / se não se / se não
+entrada = input('Você quer "entrar" ou "sair"? ')
+
+if entrada == 'entrar':
+    print('Você entrou no cistema')
+# elif entrada == 'sair':
+#     print('Você saiu do cistema')
+# else:
+# print('Você não digitou nem entrar e nem sair.')  
