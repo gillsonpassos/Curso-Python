9c000fc382e6d51af00e5b5c9328d686b2381d0d
gillsonpassos <oliveirapassos07@outlook.com>
2023-11-01 10:23:16 -0300

    if-elif e else: entendendo o fluxo do interpretador em condicionais

---
+++ aula17.py
@@ -0,0 +1,36 @@
+# if  / elif      / else
+# se / se não se / se não
+# entrada = input('Você quer "entrar" ou "sair"? ')
+
+# if entrada == 'entrar':
+#     print('Você entrou no cistema')
+
+#     print(12341234)
+# elif entrada == 'sair':
+#     print('Você saiu do cistema')
+# else:
+#     print('Você não digitou nem entrar e nem sair.')  
+
+#     print('FORA DOS BLOCOS')
+
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = False
+
+if condicao1:
+    print('Código para condição 1')
+    print('Código para condição 1')
+elif condicao2:    
+    print('Código para condição 2')
+elif condicao3:    
+    print('Código para condição 3')
+elif condicao4:    
+    print('Código para condição 4')
+
+else:
+    print('Nenhuma condição foi satisfeita.')
+    
+if 10 == 10:
+   print('outro if')
+   print('Fora do if')    
